33b07abf99b26a73c452736ed9b2976a3ab82a54
villi02 <villiarnar1@gmail.com>
2022-02-26 00:57:14 +0100

    Added final project

---
+++ Final project/DivDiary/DivDiary/View Controllers/SignUpViewController.swift
@@ -0,0 +1,34 @@
+
+import UIKit
+
+class SignUpViewController: UIViewController {
+
+    @IBOutlet weak var firstNameTextField: UITextField!
+    
+    @IBOutlet weak var lastNameTextField: UITextField!
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var signUpButton: UIButton!
+    
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        setUpElements()
+    }
+    
+    func setUpElements() {
+        // Hides the error label
+        errorLabel.alpha = 0
+    }
+    
+    @IBAction func signUpTapped(_ sender: Any) {
+    }
+    
+
+}
